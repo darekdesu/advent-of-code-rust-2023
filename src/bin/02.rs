f47a14d9f614e77e211e0dff52b3a10ea5cdfbaa
Darek Desu <4459421+darekdesu@users.noreply.github.com>
2023-12-12 18:34:39 +0100

    D2P2: Done

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -6,6 +6,10 @@ const BLUE_CUBES_LIMIT: u32 = 14;
 pub fn is_limit_reached(words: Vec<&str>, limit: u32) -> bool {
     words.get(0).unwrap_or(&"0").parse().unwrap_or(0) > limit
 }
+
+pub fn get_number_of_cubes(words: Vec<&str>) -> u32 {
+    words.get(0).unwrap_or(&"0").parse().unwrap_or(0)
+}
 pub fn part_one(input: &str) -> Option<u32> {
     let result =
         input
@@ -53,7 +57,48 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let result = input
+        .lines()
+        .map(|line| {
+            let mut game_number: u32 = 0;
+            let mut red_counter: Vec<u32> = Vec::new();
+            let mut green_counter: Vec<u32> = Vec::new();
+            let mut blue_counter: Vec<u32> = Vec::new();
+
+            line.split(|c| c == ':' || c == ';' || c == ',')
+                .for_each(|s| {
+                    let game_with_outputs: Vec<&str> = s.trim().split_whitespace().collect();
+
+                    match s {
+                        s if s.contains("Game") => {
+                            game_number = game_with_outputs
+                                .get(1)
+                                .unwrap_or(&"0")
+                                .parse()
+                                .unwrap_or(0)
+                        }
+                        s if s.contains("red") => {
+                            red_counter.push(get_number_of_cubes(game_with_outputs))
+                        }
+                        s if s.contains("green") => {
+                            green_counter.push(get_number_of_cubes(game_with_outputs))
+                        }
+                        s if s.contains("blue") => {
+                            blue_counter.push(get_number_of_cubes(game_with_outputs))
+                        }
+                        _ => {}
+                    }
+                });
+
+            let minimum_red_counter = red_counter.iter().max().unwrap_or(&0);
+            let minimum_green_counter = green_counter.iter().max().unwrap_or(&0);
+            let minimum_blue_counter = blue_counter.iter().max().unwrap_or(&0);
+
+            minimum_red_counter * minimum_green_counter * minimum_blue_counter
+        })
+        .sum();
+
+    Some(result)
 }
 
 #[cfg(test)]
@@ -69,6 +114,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2286));
     }
 }
