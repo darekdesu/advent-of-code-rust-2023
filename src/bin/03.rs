c30a2f7d8dd8bd395ef67e364044d2f02d15dfe9
Darek Desu <4459421+darekdesu@users.noreply.github.com>
2023-12-17 21:24:58 +0100

    D3P2: Solution

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,8 +1,20 @@
+use std::collections::HashMap;
 advent_of_code::solution!(3);
 
 pub fn is_engine_number(value: char) -> bool {
     value != '.' && value != 'X'
 }
+
+const NEIGHBORS: [[isize; 2]; 8] = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+];
 
 pub fn part_one(input: &str) -> Option<u32> {
     let size = input.lines().count() + 2;
@@ -21,17 +33,6 @@ pub fn part_one(input: &str) -> Option<u32> {
         }
     });
 
-    let neighbors: [[isize; 2]; 8] = [
-        [-1, -1],
-        [-1, 0],
-        [-1, 1],
-        [0, -1],
-        [0, 1],
-        [1, -1],
-        [1, 0],
-        [1, 1],
-    ];
-
     let mut found_values_list = Vec::new();
 
     for i in 1..size - 1 {
@@ -44,7 +45,7 @@ pub fn part_one(input: &str) -> Option<u32> {
             if is_engine_number(current) {
                 temporary_value.push(current);
 
-                neighbors.iter().for_each(|[x, y]| {
+                NEIGHBORS.iter().for_each(|[x, y]| {
                     let new_i = i as isize + x;
                     let new_j = j as isize + y;
                     let neighbor = grid[new_i as usize][new_j as usize];
@@ -75,8 +76,92 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(result)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn update_found_values_list(
+    found_values_list: &mut HashMap<String, Vec<String>>,
+    gear_coordinates: String,
+    temporary_value: String,
+) {
+    match found_values_list.get_mut(&gear_coordinates) {
+        Some(found_values_list) => {
+            found_values_list.push(temporary_value.clone());
+        }
+        None => {
+            found_values_list.insert(gear_coordinates.clone(), vec![temporary_value.clone()]);
+        }
+    };
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let size = input.lines().count() + 2;
+    let mut grid = vec![vec!['.'; size]; size];
+
+    input.lines().enumerate().for_each(|(col_idx, line)| {
+        for (row_idx, char) in line.chars().enumerate() {
+            if char.is_ascii_digit() {
+                grid[col_idx + 1][row_idx + 1] = char;
+                continue;
+            }
+
+            if char == '*' {
+                grid[col_idx + 1][row_idx + 1] = 'X';
+            }
+        }
+    });
+
+    let mut found_values_list: HashMap<String, Vec<String>> = HashMap::new();
+
+    for i in 1..size - 1 {
+        let mut temporary_value = String::new();
+        let mut gear_coordinates = String::new();
+        for j in 1..size - 1 {
+            let current = grid[i][j];
+            let is_last = j == (size - 2);
+
+            if is_engine_number(current) {
+                NEIGHBORS.iter().for_each(|[x, y]| {
+                    let new_i = i as isize + x;
+                    let new_j = j as isize + y;
+                    let neighbor = grid[new_i as usize][new_j as usize];
+
+                    if neighbor == 'X' {
+                        gear_coordinates = format!("{}-{}", new_i, new_j);
+                    }
+                });
+
+                temporary_value.push(current);
+            } else if !gear_coordinates.is_empty() {
+                update_found_values_list(
+                    &mut found_values_list,
+                    gear_coordinates.clone(),
+                    temporary_value.clone(),
+                );
+                gear_coordinates.clear();
+                temporary_value.clear();
+            } else if gear_coordinates.is_empty() {
+                temporary_value.clear();
+            }
+
+            if is_last && !gear_coordinates.is_empty() {
+                update_found_values_list(
+                    &mut found_values_list,
+                    gear_coordinates.clone(),
+                    temporary_value.clone(),
+                );
+            }
+        }
+    }
+
+    let result = found_values_list
+        .values()
+        .filter(|v| v.len() == 2)
+        .fold(0, |acc, values| {
+            acc + values
+                .iter()
+                .map(|v| v.parse::<u32>().unwrap_or(0))
+                .product::<u32>()
+        });
+
+    Some(result)
 }
 
 #[cfg(test)]
@@ -92,6 +177,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6756));
     }
 }
